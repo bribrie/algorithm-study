82135a0ae195d2f0a343069886b227af488fc39d
miraecod <miraecod@gmail.com>
2022-04-06 21:46:58 +0900

    🥺 [Day 6] 문자열 내 p와 y의 개수

---
+++ miraecod/문자열 내 p와 y의 개수.js
@@ -0,0 +1,14 @@
+// ? https://programmers.co.kr/learn/courses/30/lessons/12916
+
+function solution(s) {
+  let p = 0;
+  let y = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "p" || s[i] === "P") p++;
+    else if (s[i] === "y" || s[i] === "Y") y++;
+  }
+
+  if (p === y) return true;
+  else if (p !== y) return false;
+}
