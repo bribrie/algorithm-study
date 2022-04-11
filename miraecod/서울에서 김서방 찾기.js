b90cdf822780a91c7b2325869ed54c134ae6efdf
miraecod <miraecod@gmail.com>
2022-04-11 21:12:07 +0900

    🥺 [Day 9] 서울에서 김서방 찾기

---
+++ miraecod/서울에서 김서방 찾기.js
@@ -0,0 +1,7 @@
+// ? https://programmers.co.kr/learn/courses/30/lessons/12919
+
+function solution(seoul) {
+  for (let i = 0; i < seoul.length; i++) {
+    if (seoul[i] === "Kim") return `김서방은 ${i}에 있다`;
+  }
+}
