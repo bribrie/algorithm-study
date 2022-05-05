4775dd9a373cb70b96573adf5c11a5891924f579
bribrie <ibriebriee@gmail.com>
2022-05-05 23:53:11 +0900

    😉 [Day 34] 정수 내림차순으로 배치하기

---
+++ bribrie/Programmers/정수 내림차순으로 배치하기.js
@@ -0,0 +1,17 @@
+function solution(n) {
+  return parseInt(
+    n
+      .toString()
+      .split("")
+      .sort((a, b) => b - a)
+      .join("")
+  );
+}
+
+// 다른 풀이
+function solution(n) {
+  const newN = n + "";
+  const newArr = newN.split("").sort().reverse().join("");
+
+  return +newArr;
+}
