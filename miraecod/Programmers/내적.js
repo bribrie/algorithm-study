f74fb06c5f1aab5f03fbeeeed2606f1c2f44a5a6
miraecod <miraecod@gmail.com>
2022-05-13 11:46:17 +0900

    🥺 [Day 43] 내적

---
+++ miraecod/Programmers/내적.js
@@ -0,0 +1,7 @@
+// ? https://programmers.co.kr/learn/courses/30/lessons/70128
+
+function solution(a, b) {
+  let sum = 0;
+  for (let i = 0; i < a.length; i++) sum += a[i] * b[i];
+  return sum;
+}
