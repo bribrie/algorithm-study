bf9a3e87f92b66574f08d596f76043abc02884e1
miraecod <miraecod@gmail.com>
2022-05-16 23:18:43 +0900

    🥺 [Day 44] Check If Two String Arrays are Equivalent

---
+++ miraecod/LeetCode/Check If Two String Arrays are Equivalent.js
@@ -0,0 +1,17 @@
+// ? https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+
+var arrayStringsAreEqual = function (word1, word2) {
+  let str1 = "";
+  let str2 = "";
+
+  for (let i = 0; i < word1.length; i++) str1 += word1[i];
+  for (let i = 0; i < word2.length; i++) str2 += word2[i];
+
+  return str1 === str2 ? true : false;
+};
